71763b8f1611a23135a113b12e8bedc7c9d75c49
vasanthmalaiyalan <vasanthmalaiyalan321@gmail.com>
2026-06-04 10:51:10 +0000

    1.2.additive-inverse.cpp,

---
+++ Basic Mathematics - Serge Lang/1.1.Additive Inverse/1.2.additive-inverse.cpp
@@ -0,0 +1,43 @@
+#include <iostream>
+
+int main() {
+    int a = 5;
+    int b = 3;
+
+    std::cout << "a = " << a << '\n';
+    std::cout << "b = " << b << '\n';
+
+    std::cout << "\n=== Additive Inverse ===\n";
+
+    std::cout << "a     = " << a << '\n';
+    std::cout << "-a    = " << -a << '\n';
+    std::cout << "a+(-a)= " << a + (-a) << '\n';
+
+    std::cout << "\n=== Double Minus ===\n";
+
+    std::cout << "-(-a) = " << -(-a) << '\n';
+
+    std::cout << "\n=== Subtraction ===\n";
+
+    std::cout << "a-b    = " << a - b << '\n';
+    std::cout << "a+(-b) = " << a + (-b) << '\n';
+
+    std::cout << "\n=== Commutativity ===\n";
+
+    std::cout << "a+b = " << a + b << '\n';
+    std::cout << "b+a = " << b + a << '\n';
+
+    std::cout << "\n=== Associativity ===\n";
+
+    int c = 9;
+
+    std::cout << "(a+b)+c = " << (a + b) + c << '\n';
+    std::cout << "a+(b+c) = " << a + (b + c) << '\n';
+
+    std::cout << "\n=== Negative Of A Sum ===\n";
+
+    std::cout << "-(a+b) = " << -(a + b) << '\n';
+    std::cout << "-a + (-b) = " << (-a) + (-b) << '\n';
+
+    return 0;
+ }
